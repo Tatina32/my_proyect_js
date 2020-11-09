b46efe530163c804b2ad3a9474ca2ace84039274
tatiana32 <tattyris@gmail.com>
2020-11-09 18:54:56 +0100

    cambios carrito

---
+++ jsMío/baseData.js
@@ -0,0 +1,92 @@
+class Product {
+	constructor(id, name, price, img) {
+		this.name = name;
+		this.id = id;
+		this.price = price;
+		this.img = img;
+	}
+}
+
+// Funcion que retorna un array de Product
+function getProducts() {
+	let productsAsObjects = JSON.parse(DATA_FROM_DATABASE);
+
+	//  Creo el arreglo con el metodo for..of..
+
+	// let products = [];
+	// for(element of productsAsObjects){
+	//     let product = new Product(element.id, element.name, element.price, element.stock, element.img)
+	//     products.push(product)
+	// }
+
+	//  Creo el arreglo usando map + arrow function
+	let products = productsAsObjects.map(
+		(value) => new Product(value.id, value.name, value.price, value.img)
+	);
+
+	return products;
+}
+
+// String con formato JSON para mi base de datos 
+const DATA_FROM_DATABASE = `[{
+    "id": 1,
+    "name": "Foto - America_1",
+    "price": 15,
+    "img": "../img/BañosEcuador.jpg"
+  }, {
+    "id": 2,
+    "name": "Foto - America_2",
+    "price": 10,
+    "img": "../img/grietas-Galapagos.jpg"
+  }, {
+    "id": 3,
+    "name": "Foto - America_3",
+    "price": 12,
+    "img": "../img/islaBartolomé-Galápagos.jpg"
+  }, {
+    "id": 4,
+    "name": "Foto - America_4",
+    "price": 13,
+    "img": "../img/Tortugas-Galápagos.jpg"
+  }, {
+    "id": 5,
+    "name": "Foto - America_5",
+    "price": 12,
+    "img": "../img/tortugaComiendo.jpg"
+  }, {
+    "id": 6,
+    "name": "Foto - America_6",
+    "price": 10,
+    "img": "../img/tortugasblackwhite.jpg"
+  }, {
+    "id": 7,
+    "name": "Foto - America_7",
+    "price": 10,
+    "img": "../img/salinas-Galápagos.jpg"
+  }, {
+    "id": 8,
+    "name": "Foto - America_8",
+    "price": 10,
+    "img": "../img/ríoPastaza-Ecuador.jpg"
+  }, {
+    "id": 9,
+    "name": "Foto - America_9",
+    "price": 11,
+    "img": "../img/pinguinos-Galápagos.jpg"
+  }, {
+    "id": 10,
+    "name": "Foto - America_10",
+    "price": 10,
+    "img": "../img/Mono.jpg"
+  }, {
+    "id": 11,
+    "name": "Foto - America_11",
+    "price": 8,
+    "img": "../img/loboMarino-SantaCruz.jpg"
+  }, {
+    "id": 12,
+    "name": "Foto - America_12",
+    "price": 9,
+    "img": "../img/iguanaMarina.jpg"
+  }]
+  `;
---
+++ jsMío/products.js
@@ -0,0 +1,107 @@
+
+const products = getProducts();
+
+// Array Carrito
+const cart = [];
+getProductsFromLocalStorage();
+
+const productsContainer = document.getElementById("productsContainer");
+productsContainer.innerHTML = "";
+let containerContent = "";
+
+
+products.forEach((product) => {
+	let productCard = createProductCardCombination(product);
+	productsContainer.appendChild(productCard);
+});
+
+/*La carta combinando ambos metodos*/
+function createProductCardCombination(product) {
+	let productCard = document.createElement("div");
+	productCard.id = product.id;
+	productCard.classList ="productCard col-4 p-2 d-flex flex-column align-items-center justify-items-center";
+
+	let code = `
+			<div>${[product.name]}</div>
+			<img src='${product.img}', width="200" height="200"/>
+			<div>Precio : $${[product.price]}</div>
+			
+	`;
+
+	let button = document.createElement("button");
+	button.innerHTML = "Agregar al carrito";
+
+	button.addEventListener("click", () => {
+		cart.push(product);
+		localStorage.setItem("cart", JSON.stringify(cart));
+	});
+
+	productCard.innerHTML = code;
+	productCard.appendChild(button);
+	return productCard;
+}
+
+/*La carta solamente con codigo HTML*/
+/*function createProductCardWithString(product) {
+	let code = `
+		<div id='${
+			product.id
+		}' class='productCard col-4 p-2 d-flex flex-column align-items-center justify-items-center width="200" height="200">
+			<div>${[product.name]}</div>
+			<img src='${product.img} '/>
+			<div>El precio del producto es $${[product.price]}</div>
+			<button class='productButton'>Agregar al carrito</button>		
+		</div>
+	`;
+	return code;
+}
+
+/*La carta solamente con elementos de Javascript*/
+/*function createProductCard(product) {
+	let productCard = document.createElement("div");
+	productCard.id = product.id;
+	productCard.classList =
+		"productCard col-4 p-2 d-flex flex-column align-items-center justify-items-center";
+
+	let nameDiv = document.createElement("div");
+	nameDiv.innerHTML = product.name;
+
+	let imgDiv = document.createElement("img");
+	imgDiv.src = product.img;
+
+	let priceDiv = document.createElement("div");
+	priceDiv.innerHTML = " Precio : $" + product.price;
+
+	let button = document.createElement("button");
+	button.innerHTML = "Agregar al carrito";
+
+	button.addEventListener("click", () => {
+		cart.push(product);
+		console.log(cart);
+	});
+
+	productCard.appendChild(nameDiv);
+	productCard.appendChild(imgDiv);
+	productCard.appendChild(priceDiv);
+	productCard.appendChild(button);
+	return productCard;
+}*/
+
+
+function getProductsFromLocalStorage() {
+	const cartFromLocalStorage = localStorage.getItem("cart");
+	if (cartFromLocalStorage) {
+		const objectsFromLocalStorage = JSON.parse(cartFromLocalStorage);
+		objectsFromLocalStorage.forEach((object) => {
+			let product = new Product(
+				object.id,
+				object.name,
+				object.price,
+				object.img,
+			);
+			cart.push(product);
+		});
+	} else {
+		console.log("ENTRAMOS AL ELSE");
+	}
+}
